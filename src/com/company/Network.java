d05fd3e5f8e8245881b031c7cfa437e0599c0d4b
Onnwen <onnwen.cassitto@icloud.com>
2022-03-02 08:57:12 +0100

    Implementazione

--- src/com/company/Network.java
+++ src/com/company/Network.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.UUID;
+
 public class Network {
     private Device mainDevice;
     private Device[] devices;
@@ -18,7 +20,17 @@ public class Network {
     }
 
     public void print() {
-        print(mainDevice, new Device[2]);
+        System.out.println("Selezionare il metodo d'input:");
+        System.out.println("[1] Esplora rete");
+        System.out.println("[2] Inserimento UUID manuale");
+        switch (Read.getInt(1, 2)) {
+            case 1:
+                exploreNetwork(mainDevice, new Device[2]);
+                break;
+            default:
+                userInputByUUID();
+                break;
+        }
     }
 
     public void setMainDevice(Device mainDevice) {
@@ -29,7 +41,38 @@ public class Network {
         return devices;
     }
 
-    private void print(Device device, Device[] selectedDevices) {
+    public Device getDevice(UUID uuid) {
+        for(Device device:getDevices()) {
+            if (device.getId().toString().equals(uuid.toString())) {
+                return device;
+            }
+        }
+        return null;
+    }
+
+    private void userInputByUUID() {
+        Device one = null;
+        System.out.println("Digitare l'UUID del primo dispositivo:");
+        do {
+            one = getDevice(Read.getUUID());
+            if (one == null) {
+                System.out.println("\uDBC0\uDD84 Il dispositivo non è stato trovato.");
+            }
+        } while (one == null);
+
+        Device two = null;
+        System.out.println("Digitare l'UUID del secondo dispositivo:");
+        do {
+            two = getDevice(Read.getUUID());
+            if (two == null) {
+                System.out.println("\uDBC0\uDD84 Il dispositivo non è stato trovato.");
+            }
+        } while (two == null);
+
+        estimateRoute(one, two).print();
+    }
+
+    private void exploreNetwork(Device device, Device[] selectedDevices) {
         Route[] routes = device.getLinkedRoutes();
 
         System.out.println("-------------------------------------------");
@@ -46,14 +89,14 @@ public class Network {
             if (input < 0) {
                 if (selectedDevices[0] == null) {
                     selectedDevices[0] = routes[input*-1 - 1].getNextDevice(device);
-                    print(routes[input*-1 - 1].getNextDevice(device), selectedDevices);
+                    exploreNetwork(routes[input*-1 - 1].getNextDevice(device), selectedDevices);
                 } else {
                     selectedDevices[1] = routes[input*-1 - 1].getNextDevice(device);
                     estimateRoute(selectedDevices[0], selectedDevices[1]).print();
                 }
             }
             else {
-                print(routes[input - 1].getNextDevice(device), selectedDevices);
+                exploreNetwork(routes[input - 1].getNextDevice(device), selectedDevices);
             }
         }
         else {
